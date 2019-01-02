fa6488a727daf051d03f77a356a6239e2b31df52
tonyikh <tonyikh@gmail.com>
2019-01-02 16:53:31 +0800

    Update resolveProxyHost.js

--- app/steps/resolveProxyHost.js
+++ app/steps/resolveProxyHost.js
@@ -12,6 +12,7 @@ function resolveProxyHost(container) {
 
   container.proxy.reqBuilder.host = parsedHost.host;
   container.proxy.reqBuilder.port = container.options.port || parsedHost.port;
+  container.proxy.reqBuilder.path = parsedHost.path;
   container.proxy.requestModule = parsedHost.module;
   return Promise.resolve(container);
 }
